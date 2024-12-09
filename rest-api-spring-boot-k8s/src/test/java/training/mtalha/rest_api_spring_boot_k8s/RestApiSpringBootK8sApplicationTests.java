46009b4e3cef0c4d9c97ee99cffa136d04ade183
Mohamed TALHA <142179779+Cloud-Elit@users.noreply.github.com>
2024-12-09 16:17:00 +0100

    Add files via upload

---
+++ rest-api-spring-boot-k8s/src/test/java/training/mtalha/rest_api_spring_boot_k8s/RestApiSpringBootK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package training.mtalha.rest_api_spring_boot_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
