ba8ae57645c66376d2b297f37dcbee4fddb045e1
Mohamed TALHA <142179779+Cloud-Elit@users.noreply.github.com>
2024-12-10 12:16:40 +0100

    Update say_hello_three.js

--- say_hello_three.js
+++ say_hello_three.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, je suis un script JS!\n');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
